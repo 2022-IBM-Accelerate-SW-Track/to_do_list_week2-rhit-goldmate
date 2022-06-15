ffa7422aed291c37f75ee75cec491656fe55ecc0
rhit-goldmate <goldmate@rose-hulman.edu>
2022-06-15 18:35:57 -0500

    project setup and importing my About.js pg / img

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,36 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import headshot from "../assets/headshot.jpg";
 
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            src={headshot}
+            alt="Profile Pic"
             ></img>
-          </div>
         </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
+      </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Taylor Goldman</div>
+          <div className="brief_description">
+          I will be a junior Computer Science major at Rose-Hulman Institute of Technology.
+           I'm currently an intern at CUNA Mutual Group in Madison, Wisconsin as an Infrastructure
+           Automation Intern in the IT department. I am also focusing my schooling on cybersecurity, 
+           as I am hoping to be able to pursue that in the future. I am looking forward to participating
+           in the IBM Accelerate Program and I hope to learn a lot to add to my skillset, and to learn
+           more about IBM. I also play on the women's tennis team at Rose-Hulman, and I 
+           enjoy going outside, and spending time with friends and family.
           </div>
         </div>
       </div>
-    );
+    </div>
+    )
   }
 }
