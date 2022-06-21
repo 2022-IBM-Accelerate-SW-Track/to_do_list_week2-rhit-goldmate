214ea8056845069a56c2e9e16b1b0a2698a5b87d
rhit-goldmate <goldmate@rose-hulman.edu>
2022-06-20 22:33:03 -0500

    finished lab

--- src/component/todos.js
+++ src/component/todos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../component/todos.css";
-import { Card, CardContent, Grid } from "@mui/material";
+import { Card, CardContent, Grid, ListItemButton, ListItemText, Checkbox } from "@mui/material";
 
 // 1. This component formats and returns the list of todos.
 // 2. Treat the question mark like an if statement.
@@ -10,6 +10,9 @@ import { Card, CardContent, Grid } from "@mui/material";
 // 4. Think of lines 14-23 as a loop. For each todo in the todo list, we want to give the list item
 // a key, and it's own card shown in the UI
 const Todos = ({ todos }) => {
+  const deleteTodo = () => {
+    deleteTodo()
+  }
   const todoList = todos.length ? (
     todos.map((todo) => {
       return (
@@ -18,7 +21,8 @@ const Todos = ({ todos }) => {
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} */}
             <ListItemButton component="a" href="#simple-list">
-              <ListItemText primary={todo.content} secondary={todo.date}/>
+              <Checkbox style={"paddingLeft:0;"} color={"primary"} />
+              <ListItemText primary={todo.content} secondary={todo.date} onclick={deleteTodo(todo.id)}/>
             </ListItemButton>
           </Card>
         </Grid>
